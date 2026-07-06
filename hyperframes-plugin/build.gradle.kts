c0e3b295826bb042957ba48caf973a3fd4a377c3
cheroliv <cheroliv@users.noreply.github.com>
2026-07-06 19:40:38 +0200

    chore(cnv): bump education.cccp.build.* 0.0.1 -> 0.0.2 (harmonisation conventions-gradle)

--- hyperframes-plugin/build.gradle.kts
+++ hyperframes-plugin/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     `java-library`
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.plugin.publish)
-    id("education.cccp.build.gradle-plugin") version "0.0.1"
-    id("education.cccp.build.publishing") version "0.0.1"
+    id("education.cccp.build.gradle-plugin") version "0.0.2"
+    id("education.cccp.build.publishing") version "0.0.2"
 }
 
 group = "education.cccp"
